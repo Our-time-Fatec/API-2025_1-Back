2ae8e2adaa759794683e01552da8d5423e5edcd8
andreluke <andre.l.sales2706@gmail.com>
2025-05-08 21:33:24 -0300

    :recycle: refactor: Melhoria na rota

--- src/controllers/QueimadaController.ts
+++ src/controllers/QueimadaController.ts
@@ -1,27 +1,31 @@
 import { CustomError } from '#/errors/custom/CustomError'
-import { array } from 'zod'
 
-type Queimada = {
-    id: string,
-    date: Date,
-    bbox: number[] 
+export type Queimada = {
+  id: string
+  date: Date
+  bbox: number[]
 }
 
-const MOCK_QUEIMADA: Queimada[] =[
-    { id: '1', date: new Date('2025-01-01'), bbox: [-46.2, -23.6, -43,9, -21.9]}
+export type GetQueimadasProps = {
+  date?: Date
+}
+
+const MOCK_QUEIMADA: Queimada[] = [
+  { id: '1', date: new Date('2025-01-01'), bbox: [-46.2, -23.6, -43.9, -21.9] },
 ]
 
 export class QueimadaController {
-  async getQueimadas(): Promise<Queimada[]> {
-    return MOCK_QUEIMADA
-  }
+  async getQueimadas({ date }: GetQueimadasProps): Promise<Queimada[]> {
+    const queimadas = MOCK_QUEIMADA
 
-  async getQueimadasData(date: Date): Promise<Queimada[]> {
-    const queimadas = await this.getQueimadas()
-    const filtered = queimadas.filter(queimada => queimada.date === date)
+    if (!date) return queimadas
+
+    const filtered = queimadas.filter(q => q.date.getTime() === date.getTime())
+
     if (filtered.length === 0) {
       throw new CustomError('Nenhum dado encontrado', 404)
     }
+
     return filtered
   }
 }
--- src/routes/queimada/queimada.ts
+++ src/routes/queimada/queimada.ts
@@ -5,55 +5,47 @@ import { StatusCodes } from '#/enums/status-code'
 import { catchError } from '#/utils/catchError'
 
 export const queimadaRoute: FastifyPluginAsyncZod = async app => {
-    app.get(
-        '/queimadas',
-        {
-            schema: {
-                summary: 'Lista de queimadas registradas, geral ou por data.',
-                tags: ['Queimadas'],
-                operationId: 'getQueimadas',
-                querystring: z.object({
-                    date: z.string().optional().refine(val => !val || !isNaN(Date.parse(val)), {
-                        message: 'Formato de data invalido'
-                    }),
-                }),
-                response: {
-                    [StatusCodes.OK]: z.object({
-                        queimadas: z.array(
-                            z.object({
-                                id: z.string(),
-                                date: z.date(),
-                                bbox: z.array(z.number())
-                            })
-                        ),
-                    }),
-                    [StatusCodes.INTERNAL_SERVER_ERROR]: z.object({
-                        message: z.string(),
-                    }),
-                },
-            },
+  app.get(
+    '/queimadas',
+    {
+      schema: {
+        summary: 'Lista de queimadas registradas, geral ou por data.',
+        tags: ['Queimadas'],
+        operationId: 'getQueimadas',
+        querystring: z.object({
+          date: z.coerce.date().optional(),
+        }),
+        response: {
+          [StatusCodes.OK]: z.object({
+            queimadas: z.array(
+              z.object({
+                id: z.string(),
+                date: z.date(),
+                bbox: z.array(z.number()),
+              })
+            ),
+          }),
+          [StatusCodes.INTERNAL_SERVER_ERROR]: z.object({
+            message: z.string(),
+          }),
         },
-        async (request, reply) => {
-            const controller = new QueimadaController()
-            const { date } = request.query as { date?: string}
+      },
+    },
+    async (request, reply) => {
+      const controller = new QueimadaController()
+      const { date } = request.query
 
-            let error, data
+      const [error, data] = await catchError(controller.getQueimadas({ date }))
 
-            if (date) {
-                [error, data] = await catchError(controller.getQueimadas())
-            } else {
-                [error, data] = await catchError(controller.getQueimadas())
-            }
+      if (error) {
+        return reply.status(error.statusCode).send({
+          message: error.message,
+        })
+      }
 
-            if(error) {
-                return reply.status(error.statusCode).send({
-                    message: error.message,
-                })
-            }
-            
-            return reply.status(StatusCodes.OK).send({
-                queimadas: data ?? [],
-            })
-        }
-    )
+      return reply.status(StatusCodes.OK).send({
+        queimadas: data,
+      })
+    }
+  )
 }
