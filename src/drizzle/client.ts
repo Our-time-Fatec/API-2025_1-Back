73472771ae6e4f5b0cc73a5a4419fe4409ecd190
andreluke <andre.l.sales2706@gmail.com>
2025-05-20 19:34:04 -0300

    :bug: fix: Alteração do client

--- src/drizzle/client.ts
+++ src/drizzle/client.ts
@@ -1,10 +1,10 @@
 import { drizzle } from 'drizzle-orm/postgres-js'
 import postgres from 'postgres'
 import { env } from '../settings/env'
-import { processImage } from './schemas/processImage'
-import { uploads } from './schemas/uploads'
-
 import { image } from './schemas/image'
+import { processImage } from './schemas/processImage'
+import { scarImage } from './schemas/scar'
+import { uploads } from './schemas/uploads'
 
 export const pg = postgres(env.POSTGRES_URL, {})
 export const db = drizzle(pg, {
@@ -12,5 +12,6 @@ export const db = drizzle(pg, {
     processImage,
     uploads,
     image,
+    scarImage,
   },
 })
